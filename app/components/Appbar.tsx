6e874c572b8d07b4620f43f536c24e56d2d7c04e
psychic-coder <rohitganguly450@gmail.com>
2024-08-31 07:50:36 +0530

    setting the authprovider

--- app/components/Appbar.tsx
+++ app/components/Appbar.tsx
@@ -1,21 +1,26 @@
 "use client";
-import { signIn } from 'next-auth/react'
-import React from 'react'
+import { signIn, signOut, useSession } from "next-auth/react";
+import React from "react";
 
 const Appbar = () => {
+  const session = useSession();
   return (
-    <div className="flex justify-between " 
-    >
-        <div>
-            Muzi
-        </div>
-        <div>
-            <button className="m-2 p-2 bg-blue-400" onClick={()=>signIn()}>
-                    Signin
-            </button>
-        </div>
+    <div className="flex justify-between ">
+      <div>Muzi</div>
+      <div>
+        {session.data?.user && (
+          <button className="m-2 p-2 bg-blue-400" onClick={() => signOut()}>
+            Logout
+          </button>
+        )}
+        {!session.data?.user && (
+          <button className="m-2 p-2 bg-blue-400" onClick={() => signIn()}>
+            Signin
+          </button>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Appbar
+export default Appbar;
