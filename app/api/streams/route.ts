90cf03de101509c2bbcd45ba8e8886e0b812efec
psychic-coder <rohitganguly450@gmail.com>
2024-08-31 09:23:30 +0530

    completed the schema and prisma setup

---
+++ app/api/streams/route.ts
@@ -0,0 +1,30 @@
+import { prismaClient } from "@/app/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+import {z} from "zod";
+
+const CreateStreamSchema=z.object({
+    creatorId:z.string(),
+    url:z.string()
+
+})
+
+export async function POST (req:NextRequest){
+    //the below code is used for data correction
+    
+    try {
+        const data= CreateStreamSchema.parse(await req.json());
+        prismaClient.stream.create({
+            userId:data.creatorId
+
+        })
+
+    } catch (error) {
+       return NextResponse.json({
+        message:"Error while adding a stream "
+       },{
+        status:411
+       })
+        
+    }
+    
+}
