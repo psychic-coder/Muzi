c5a95cb5feac3fd94f9f5eb1411845f6be9f1510
psychic-coder <rohitganguly450@gmail.com>
2024-09-02 15:36:53 +0530

    modified the prisma and made the backend

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -2,12 +2,16 @@ import { prismaClient } from "@/app/lib/db";
 import { YT_REGEX } from "@/app/lib/utils";
 import { NextRequest, NextResponse } from "next/server";
 import {z} from "zod";
+//@ts-ignore
+import youtubesearchapi from "youtube-search-api";
 
 const CreateStreamSchema=z.object({
     creatorId:z.string(),
     url:z.string()
 
 })
+const MAX_QUEUE_LEN = 20;
+
 const YT=new RegExp(YT_REGEX);
 
 
@@ -15,30 +19,53 @@ export async function POST (req:NextRequest){
     //the below code is used for data correction
     
     try {
-        const data= CreateStreamSchema.parse(await req.json());
-        const isYt=YT.test(data.url);
-        
-        if(!isYt){
+        const data = CreateStreamSchema.parse(await req.json());
+        const isYt = data.url.match(YT_REGEX)
+        if (!isYt) {
             return NextResponse.json({
-                message:"Wrong URL format "
-               },{
-                status:411
-               }) 
+                message: "Wrong URL format"
+            }, {
+                status: 411
+            })    
         }
 
+        const extractedId = data.url.split("?v=")[1];
+        const res = await youtubesearchapi.GetVideoDetails(extractedId);
 
-        //it is the extracted id from the url we have 
-        const extractedId=data.url.split("?v=")[1];
+        const thumbnails = res.thumbnail.thumbnails;
+        thumbnails.sort((a: {width: number}, b: {width: number}) => a.width < b.width ? -1 : 1);
 
-          await prismaClient.stream.create({
-           data:{
-            userId:data.creatorId,
-            url:data.url,
-            extractedId,
-            type:"Youtube"
-           }
+        const existingActiveStream = await prismaClient.stream.count({
+            where: {
+                userId: data.creatorId
+            }
         })
 
+        if (existingActiveStream > MAX_QUEUE_LEN) {
+            return NextResponse.json({
+                message: "Already at limit"
+            }, {
+                status: 411
+            })
+        }
+
+        const stream = await prismaClient.stream.create({
+            data: {
+                userId: data.creatorId,
+                url: data.url,
+                extractedId,
+                type: "Youtube",
+                title: res.title ?? "Cant find video",
+                smallImg: (thumbnails.length > 1 ? thumbnails[thumbnails.length - 2].url : thumbnails[thumbnails.length - 1].url) ?? "https://cdn.pixabay.com/photo/2024/02/28/07/42/european-shorthair-8601492_640.jpg",
+                bigImg: thumbnails[thumbnails.length - 1].url ?? "https://cdn.pixabay.com/photo/2024/02/28/07/42/european-shorthair-8601492_640.jpg"
+            }
+        });
+
+        return NextResponse.json({
+            ...stream,
+            hasUpvoted: false,
+            upvotes: 0
+        })
     } catch (error) {
        return NextResponse.json({
         message:"Error while adding a stream "
---
+++ prisma/migrations/20240902100409_title/migration.sql
@@ -0,0 +1,21 @@
+-- AlterTable
+ALTER TABLE "Stream" ADD COLUMN     "bigImg" TEXT NOT NULL DEFAULT '',
+ADD COLUMN     "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "played" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN     "playedTs" TIMESTAMP(3),
+ADD COLUMN     "smallImg" TEXT NOT NULL DEFAULT '',
+ALTER COLUMN "title" SET DEFAULT '';
+
+-- CreateTable
+CREATE TABLE "CurrentStream" (
+    "userId" TEXT NOT NULL,
+    "streamId" TEXT,
+
+    CONSTRAINT "CurrentStream_pkey" PRIMARY KEY ("userId")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "CurrentStream_streamId_key" ON "CurrentStream"("streamId");
+
+-- AddForeignKey
+ALTER TABLE "CurrentStream" ADD CONSTRAINT "CurrentStream_streamId_fkey" FOREIGN KEY ("streamId") REFERENCES "Stream"("id") ON DELETE SET NULL ON UPDATE CASCADE;
