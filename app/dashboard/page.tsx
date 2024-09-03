c30a2925e3b0b5a735108cbef91016c933806d39
psychic-coder <rohitganguly450@gmail.com>
2024-09-03 16:21:45 +0530

    made the streaming website

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+"use client"
+import { useSession } from 'next-auth/react'
 
-const page = () => {
-  return (
-    <div>Dashboard</div>
-  )
+import StreamView from '@/app/components/StreamView'
+import useRedirect from '@/app/hooks/useRedirect';
+
+export default function Component() {
+    const session = useSession();
+    const redirect = useRedirect();
+    const user=session.data?.user;
+    console.log(session);
+    
+    try {
+        if (!user) {
+            return (
+                <h1>Please Log in....</h1>
+            )
+        }
+     return <StreamView creatorId={session.data?.user?.email} playVideo={true} />
+       
+    } catch(e) {
+        return null
+    }
 }
 
-export default page
+export const dynamic = 'auto'
